b4b19e868dec87757b9b79791e8a4308c9dce2b8
Alan Diaz <adiazny@gmail.com>
2022-04-10 18:38:06 -0400

    add middleware plumbing - wrapping handlers

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ new strava uploader
 # Project Layout
 
 ## Foundation
-The foudation package is used as a library package to be used in the business packages.
+The foudation packages are used as a library packages to be used in the business packages. Think of them as our home grown standard library. No logging is done in any of the foundation packages.
 
 ### Web
 The web package is a mini web framwork built to customize the handler functions for overwritting / extending the current mux handle implemenation to make it our own. 
+
+### Middleware
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -20,13 +20,15 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*httptreemux.ContextMux //App emeds ContexMux which makes App everything a ContextMux is.
 	shutdown                chan os.Signal
+	mw                      []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	return &App{
 		httptreemux.NewContextMux(),
 		shutdown,
+		mw,
 	}
 }
 
@@ -38,17 +40,26 @@ func (a *App) SignalShutdown() {
 
 // Handle sets a handler function for a given HTTP method and path pair
 // to the application server mux.
-func (a *App) Handle(method string, group string, path string, handler Handler) {
-	h := func(w http.ResponseWriter, r *http.Request) {
+func (a *App) Handle(method string, group string, path string, handler Handler, mw ...Middleware) {
 
-		// PRE CODE PROCESSING HERE
+	// First wrap handler specific middleware around this handler.
+	handler = wrapMiddleware(mw, handler)
 
-		if err := handler(r.Context(), w, r); err != nil {
-			//ERROR HANDLING HERE
+	// Add the application's general middleware to the handler chain.
+	handler = wrapMiddleware(a.mw, handler)
+
+	// The function to execute for each request.
+	h := func(w http.ResponseWriter, r *http.Request) { // this is the outter onion handler
+
+		//INJECT CODE
+
+		// Call the wrapped handler functions.
+		if err := handler(r.Context(), w, r); err != nil { //this is our inner onion handle i.e. the testgrp.Test handler
+			// INJECT CODE
 			return
 		}
 
-		// POST CODE PROCESSING HERE
+		// INJECT CODE
 	}
 
 	finalPath := path
